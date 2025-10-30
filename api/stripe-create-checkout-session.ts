6534ab58d2d9f366fb286f7e75e3c2713a2fa573
Zoé Mateus <83597652+Zoemateus324@users.noreply.github.com>
2025-10-30 17:04:11 -0300

    Atualizar o stripe-create-checkout-session.ts

--- api/stripe-create-checkout-session.ts
+++ api/stripe-create-checkout-session.ts
@@ -41,7 +41,7 @@ export default async function handler(req: VercelRequest, res: VercelResponse) {
 
     // Criar Checkout Session
     const session = await stripe.checkout.sessions.create({
-      payment_method_types: ['card', 'pix'],
+      payment_method_types: ['card', 'pix, 'ticket'],
       line_items: [
         {
           price: priceId,
