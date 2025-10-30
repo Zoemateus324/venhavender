27d8a258a95d819c35f35dd82a9f384c19bd7364
Zoé Mateus <83597652+Zoemateus324@users.noreply.github.com>
2025-10-30 16:54:56 -0300

    Atualizar o stripe-create-payment-intent.ts

--- api/stripe-create-payment-intent.ts
+++ api/stripe-create-payment-intent.ts
@@ -2,7 +2,8 @@ import type { VercelRequest, VercelResponse } from '@vercel/node';
 import Stripe from 'stripe';
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: '2024-06-20',
+  apiVersion: '2025-09-30.clover',
+
 });
 
 export default async function handler(req: VercelRequest, res: VercelResponse) {
